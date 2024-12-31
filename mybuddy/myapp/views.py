c7e3cad18af19aa12894344b46c1b66fa472aef9
ashurohom <ashitosh.rohom@gmail.com>
2024-12-31 12:30:14 +0530

    update css for index.html

--- mybuddy/myapp/views.py
+++ mybuddy/myapp/views.py
@@ -19,4 +19,4 @@ def viewpetdetails(request):
     return render(request,'viewpetdetails.html')
 
 def about(request):
-    return render(request,'header.html')
+    return render(request,'base.html')
