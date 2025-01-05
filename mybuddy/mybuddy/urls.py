2e5cde4b9196f956048c944f048c7c4c93a9ffc7
ashurohom <ashitosh.rohom@gmail.com>
2025-01-05 19:14:17 +0530

    add urls

--- mybuddy/mybuddy/urls.py
+++ mybuddy/mybuddy/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +32,4 @@ urlpatterns = [
     path('contact/',views.contact),
     path('donate/',views.donate),
 ]
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
