a220a950abd85d548d317491ca9e3fdba6c2dc0a
ashurohom <ashitosh.rohom@gmail.com>
2025-01-16 19:13:59 +0530

    update request

--- mybuddy/myapp/admin.py
+++ mybuddy/myapp/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import Pet
+from .models import Adoptionrequest
+
 
 # Register your models here.
 admin.site.register(Pet)
 
+
+
+
+class AdoptionRequestAdmin(admin.ModelAdmin):
+    list_display = ['pet_name','pet_breed', 'full_name', 'status']
+    actions = ['approve_request', 'reject_request']
+
+    def approve_request(self, request, queryset):
+        queryset.update(status='Approved')
+    
+    def reject_request(self, request, queryset):
+        queryset.update(status='Rejected')
+
+admin.site.register(Adoptionrequest, AdoptionRequestAdmin)
