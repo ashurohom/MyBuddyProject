e4ef5d873131671ffbcfea15ee26808153fbe911
ashurohom <ashitosh.rohom@gmail.com>
2025-02-08 23:11:48 +0530

    update admin

--- mybuddy/myapp/models.py
+++ mybuddy/myapp/models.py
@@ -71,3 +71,9 @@ class Contact(models.Model):
     number=models.CharField(max_length=10)
     message=models.CharField(max_length=300)
     
+
+class AdoptionHistory(models.Model):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE,db_column="user")
+    pet_name = models.CharField(max_length=255)
+    pet_breed = models.CharField(max_length=255)
+    adoption_date = models.DateTimeField(auto_now_add=True)
